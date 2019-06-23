df7f60ee4ea5e548325daf38394775a44750f178
Claire Laville <clearthecity@gmail.com>
2019-06-22 19:20:21 -0700

    multiform layout

--- src/main.js
+++ src/main.js
@@ -6,8 +6,14 @@ import store from "./store"
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import upperFirst from 'lodash/upperFirst'
-import camelCase from 'lodash/camelCase'
+
+/*
+ To run Python scripts: https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
+https://www.ivarprudnikov.com/nodejs-server-running-python-scripts/
+
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn('python',["path/to/script.py", arg1, arg2, ...]);
+*/
 
 Vue.config.productionTip = false;
 
@@ -16,35 +22,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
-
-
-/*
-  https://vuejs.org/v2/guide/components-registration.html
-*/
-const requireComponent = require.context(
-  './components', true, /Base[A-Z]\w+\.(vue|js)$/
-)
-
-requireComponent.keys().forEach(fileName => {
-  // Get component config
-  const componentConfig = requireComponent(fileName)
-  // Get PascalCase name of component
-  const componentName = upperFirst(
-    camelCase(
-      // Gets the file name regardless of folder depth
-      fileName
-        .split('/')
-        .pop()
-        .replace(/\.\w+$/, '')
-    )
-  )
-
-  // Register component globally
-  Vue.component(
-    componentName,
-    // Look for the component options on `.default`, which will
-    // exist if the component was exported with `export default`,
-    // otherwise fall back to module's root.
-    componentConfig.default || componentConfig
-  )
-});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,15 @@
 
   <div class='container-fluid'>
 
-    <div class='row'>
-  <div class='col-sm-4'>
-    <p> Blend your writing style with the style of a famous author using a Markov model </p>
-  </div>
+    <div class='container'>
+      <p> Blend your writing style with the style of a famous author using a Markov model </p>
+    </div>
 
+  <div v-if="currentWindow == 1" id='author-selection-window' class='container window'>
+    <div style='text-align:center'>
+      <h4>Step {{ currentWindow }}: Choose an author</h4>
+    </div>
+  <div class='row'>
   <div class='col'>
    <div id='author-radio' class='form-group'>
      <div v-bind:key='a' v-for='a in authors' class='form-check'>
@@ -16,29 +20,60 @@
          <Author v-bind:author-name="a['authorName']" v-bind:author-id="a['authorId']"></Author>
        </label>
      </div>
+     <div class='form-check' style='margin-top:0.5rem'>
+       <button type='button' v-bind='selectedAuthor' v-on:click='randomAuthor()'>Surprise me</button>
+    </div>
     </div>
   </div>
-
 </div>
-<div class='row'>
-  <div class='col-lg-6 col-md-8'>
-    <textarea class='form-control' v-model='inputText' rows='6' placeholder='Write something'></textarea>
-    <input type='file' class='form-control-file' name='file'>
-
-    <div class='row'><div class='col'>
-      <button type='button' v-on:click='generateFromInput' class='btn btn-dark'>Generate</button>
-      <button type='button' v-on:click="resetInputForm" class='btn btn-light'>Reset</button>
-    </div></div>
+<div class='row window-nav'>
+  <div class='col'>
+    <div class='form-group'>
+      <button type='button' v-bind:class="{disabled : (selectedAuthor == null)}" v-bind='currentWindow' v-on:click="nextWindow()" class='btn btn-dark' >Next</button>
+    </div>
   </div>
-
 </div>
+</div> <!-- end author selection window -->
 
+<div v-if="currentWindow == 2" class='container window' id='text-input-window'>
+  <div style='text-align:center'>
+    <h4>Step {{ currentWindow }}: Upload your text</h4>
+    <h5>Author model: {{ selectedAuthor }}</h5>
+  </div>
+    <div class='row'>
+      <div class='col'>
+        <textarea class='form-control' v-model='inputText' rows='6' placeholder='Write something'></textarea>
+        <input type='file' class='form-control-file' name='file'>
+      </div>
+    </div>
+        <div class='row window-nav'>
+          <div class='col'>
+            <div class='form-group'>
+            <button type='button' v-bind='currentWindow' v-on:click="previousWindow" class='btn btn-light mr-2'>Back</button>
+            <button type='button' v-on:click='generateFromInput' class='btn btn-dark mr-2'>Generate</button>
+            <button type='button' v-on:click="resetInputForm" class='btn btn-light mr-2'>Clear</button>
+            </div>
+          </div>
+        </div>
+  </div> <!-- end text input window -->
 
-  </div> <!-- end container -->
+  </div> <!-- end container-fluid -->
   </div> <!-- end home -->
 </template>
 
 <style scoped>
+
+  .window {
+      border: 1px solid #2c3e50;
+      border-radius: 10px;
+      background-color: lavender;
+      padding: 1rem;
+  }
+
+
+  button.disabled:hover {
+    cursor: not-allowed;
+  }
 </style>
 
 <script>
@@ -52,6 +87,7 @@ export default {
   },
   data() {
     return {
+      currentWindow: 1,
       authors: [
         { authorName: 'Henry James', authorId: 'HenryJames' },
         { authorName: 'Frank Norris', authorId: 'FrankNorris' },
@@ -68,11 +104,20 @@ export default {
       alert('Coming soon!')
     },
     resetInputForm: function() {
-      this.selectedAuthor = null
       this.inputText = ""
-    }
+    },
+    randomAuthor: function() {
+      let rand = Math.floor(Math.random() * this.authors.length)
+      this.selectedAuthor = this.authors[rand].authorId
+    },
+    nextWindow: function() {
+      this.currentWindow += 1
+    },
+    previousWindow: function() {
+      if (this.currentWindow > 1)
+        this.currentWindow -= 1
+    },
   },
-
 }
 
 </script>
