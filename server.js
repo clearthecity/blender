4f70b680863aa339be9ec97a32c316d364ec230a
Claire Laville <clearthecity@gmail.com>
2019-06-28 21:02:42 -0700

    build with express, server.js, dist

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = requre('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Create a static website
+app.use(express.static(__dirname + "/dist/"));
+
+// Capture all requests (SPA routes) and send to index.html
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
+//console.log('Server started');
+
+/* https://www.youtube.com/watch?v=yfW9knTBR90&list=WL&index=17&t=3s */
